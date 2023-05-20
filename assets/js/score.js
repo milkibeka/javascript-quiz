b1e58a989a11f325c708d695df103dc2e9c062e5
milkibeka <125259404+milkibeka@users.noreply.github.com>
2023-05-21 00:52:59 +0100

    Sort high scores in descending order

--- assets/js/score.js
+++ assets/js/score.js
@@ -1,2 +1,5 @@
 // Retrieve high scores from local storage
 const highscores = JSON.parse(localStorage.getItem('highscores')) || [];
+
+// Sort high scores in descending order
+highscores.sort((a, b) => b.score - a.score);
