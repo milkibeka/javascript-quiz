bd5253afca58b5f8dec9243d71d3a8b498a1482f
milkibeka <125259404+milkibeka@users.noreply.github.com>
2023-05-21 00:55:16 +0100

    scoreList is defined, createElement is applied to add to the list and testContent to show on the page.

--- assets/js/score.js
+++ assets/js/score.js
@@ -3,3 +3,11 @@ const highscores = JSON.parse(localStorage.getItem('highscores')) || [];
 
 // Sort high scores in descending order
 highscores.sort((a, b) => b.score - a.score);
+
+// Display high scores on the HTML page
+const scoresList = document.querySelector('#highscores');
+highscores.forEach((score) => {
+  const scoreElement = document.createElement('li');
+  scoreElement.textContent = `${score.initials}: ${score.score}`;
+  scoresList.appendChild(scoreElement);
+});
