7bf98dac2cc6ffbcb00c1c7abd1d3323782c6bfa
milkibeka <125259404+milkibeka@users.noreply.github.com>
2023-05-20 23:59:45 +0100

    startQuiz functions is defined

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -7,3 +7,15 @@ var startbtn= document.getElementById("start");
 
 startbtn.addEventListener("click", startQuiz);
 
+function startQuiz() {
+    
+    //Hide start screen
+       startscrn.classList.add("hide");
+       
+    // Display questions screen
+       questionscrn.classList.remove("hide");
+    
+        startTimer();
+    
+        showQuestions();
+    };
