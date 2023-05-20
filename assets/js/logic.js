9b08b4a00b566a4d5394f340ddb96920d8a0b920
milkibeka <125259404+milkibeka@users.noreply.github.com>
2023-05-20 23:58:06 +0100

    event listener is addted to start button

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,1 +1,9 @@
 var myQuestions=quizQuestions;
+var startbtn= document.getElementById("start");
+var startscrn=document.getElementById("start-screen");
+var questionscrn=document.getElementById("questions");
+var startbtn= document.getElementById("start");
+// Start Quiz
+
+startbtn.addEventListener("click", startQuiz);
+
