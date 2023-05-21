ce695f03215add20c2cf44f6ad6331bc3a416e69
milkibeka <125259404+milkibeka@users.noreply.github.com>
2023-05-21 01:21:10 +0100

    score.js path on the highscore.html is corrected. For consistency var and getElementById is used in score.js

--- assets/js/score.js
+++ assets/js/score.js
@@ -1,13 +1,13 @@
 // Retrieve high scores from local storage
-const highscores = JSON.parse(localStorage.getItem('highscores')) || [];
+var highscores = JSON.parse(localStorage.getItem('highscores')) || [];
 
 // Sort high scores in descending order
 highscores.sort((a, b) => b.score - a.score);
 
 // Display high scores on the HTML page
-const scoresList = document.querySelector('#highscores');
+var scoresList = document.getElementById("highscores");
 highscores.forEach((score) => {
-  const scoreElement = document.createElement('li');
+  var scoreElement = document.createElement('li');
   scoreElement.textContent = `${score.initials}: ${score.score}`;
   scoresList.appendChild(scoreElement);
 });
